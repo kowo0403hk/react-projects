9b045fb95a42afddd444017ea8ce74d800d777b2
Alex Cheung <alexc.sfu@gmail.com>
2022-06-19 04:22:21 -0700

    minor change of variable names for code clarity

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Categories = ({ filterItems, categories }) => {
-  const mappedButtons = categories.map((category, index) => {
+  const mappedCategories = categories.map((category, index) => {
     return (
       <button
         type="button"
@@ -14,7 +14,7 @@ const Categories = ({ filterItems, categories }) => {
     );
   });
 
-  return <div className="btn-container">{mappedButtons}</div>;
+  return <div className="btn-container">{mappedCategories}</div>;
 };
 
 export default Categories;
