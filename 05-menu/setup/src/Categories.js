506f81ae3aebf59bf58f4115c36c22d339adbcf3
Alex Cheung <alexc.sfu@gmail.com>
2022-06-19 04:17:41 -0700

    completed Menus project by implementing Set data structure to select unique value for array

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ filterItems, categories }) => {
+  const mappedButtons = categories.map((category, index) => {
+    return (
+      <button
+        type="button"
+        className="filter-btn"
+        key={index}
+        onClick={() => filterItems(category)}
+      >
+        {category}
+      </button>
+    );
+  });
+
+  return <div className="btn-container">{mappedButtons}</div>;
 };
 
 export default Categories;
--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ items }) => {
+  const mappedItems = items.map((item) => {
+    const { id, price, title, img, desc } = item;
+    return (
+      <article key={id} className="menu-item">
+        <img src={img} alt={title} className="photo" />
+        <div className="item-info">
+          <header>
+            <h4>{title}</h4>
+            <h4 className="price">${price}</h4>
+          </header>
+          <p className="item-text">{desc}</p>
+        </div>
+      </article>
+    );
+  });
+  return <div className="section-center">{mappedItems}</div>;
 };
 
 export default Menu;
