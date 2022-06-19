ccf6055a5840754ffca6e8a387f5c64b20febae8
Alex Cheung <alexc.sfu@gmail.com>
2022-06-18 21:22:41 -0700

    implemented data fetching by using axios package instead of built-in fetch package

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((res) => {
+        setTours(res.data);
+        setLoading(false);
+      })
+      .catch((e) => {
+        setLoading(false);
+        console.log(e.message);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  } else {
+    return (
+      <main>
+        <Tours tours={tours} />
+      </main>
+    );
+  }
 }
 
-export default App
+export default App;
