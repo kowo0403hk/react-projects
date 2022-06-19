cb7429deadf37166d0f42f3a1de819ec02948564
Alex Cheung <alexc.sfu@gmail.com>
2022-06-19 02:21:48 -0700

    completed Questions project by implementing ternary show/hide component logic

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  const mappedQuestions = questions.map((question) => {
+    return <SingleQuestion key={question.id} {...question} />;
+  });
+  return (
+    <main>
+      <div className="container">
+        <h3>quesitons and answers about login</h3>
+        <section className="info">{mappedQuestions}</section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
